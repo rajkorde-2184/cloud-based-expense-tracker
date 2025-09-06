0a7b7034812431e6128b667ec4f9066fca43c840
rajkorde <rajkorde68@gmail.com>
2025-09-06 15:39:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { auth } from "./firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import AddExpenseForm from "./components/AddExpenseForm";
 import ExpenseList from "./components/ExpenseList";
-import Summary from "./components/Summary";
 import AuthForm from "./components/AuthForm";
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+      setUser(currentUser); // ✅ updates on login/logout
     });
     return () => unsubscribe();
   }, []);
@@ -19,11 +18,9 @@ function App() {
   if (!user) return <AuthForm setUser={setUser} />;
 
   return (
-    <div style={{ maxWidth: "600px", margin: "auto", padding: "20px" }}>
-      <h1>💰 Cloud Expense Tracker</h1>
-      <button onClick={()=>signOut(auth)}>Logout</button>
+    <div>
+      <button onClick={() => signOut(auth)}>Logout</button>
       <AddExpenseForm user={user} />
-      <Summary user={user} />
       <ExpenseList user={user} />
     </div>
   );
